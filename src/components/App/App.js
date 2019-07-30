4b9ceb135a8b10be0d2736fbcd6b7e7428415700
Slava <slava91petrushin@yandex.ru>
2019-07-30 22:51:17 +0300

    second commit homework

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const text = '<p>Простой параграф текста</p>'
+
+const App = (props) => {
+	return (
+		<div className="App">
+			<p className="description">Миру-мир, студентам - beer.</p>
+		</div>
+	)
+};
+
+export default App;
